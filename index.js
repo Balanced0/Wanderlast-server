63c921ea301a5af9ae81cc468c119462c981930b
balanced0 <alvichowdhury013@gmail.com>
2026-05-15 20:27:06 +0600

    chore: add package.json with initial dependencies for the server

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("wanderlast");
+    const destinationCollection = db.collection("destinations");
+
+    app.post('/destination', async (req, res) =>{
+      const destinationData = req.body;
+      const result = await destinationCollection.insertOne(destinationData);
+      res.json(result);
+    });
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) =>{
+    res.send("Server is running fine");
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
